4ca4e95a859b2c3cd92c2f45de5425dc336e87bd
oleg <demokrat312@gmail.com>
2020-04-21 02:55:24 +0600

    Русский язык и рабочий стол

--- config/bundles.php
+++ config/bundles.php
@@ -22,4 +22,5 @@ return [
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Sonata\UserBundle\SonataUserBundle::class => ['all' => true],
     Symfony\Bundle\AclBundle\AclBundle::class => ['all' => true],
+    Sonata\TranslationBundle\SonataTranslationBundle::class => ['all' => true],
 ];
