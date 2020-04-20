022926da9990385e255476ef91b1703c91dcf903
oleg <demokrat312@gmail.com>
2020-04-21 00:50:20 +0600

    first commit

---
+++ src/Entity/Group.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: temerso_test
+ * Date: 19.04.2020
+ * Time: 20:15
+ */
+namespace App\Entity;
+
+use Sonata\UserBundle\Entity\BaseGroup as BaseGroup;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_group")
+ */
+class Group extends BaseGroup
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer", options={"unsigned":true})
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
