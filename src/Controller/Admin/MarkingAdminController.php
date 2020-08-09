baad7013e8f2b84feef718d289ec983040ed699a
oleg <demokrat312@gmail.com>
2020-08-09 20:23:58 +0600

    Смена статусов маркировка

--- src/Controller/Admin/MarkingAdminController.php
+++ src/Controller/Admin/MarkingAdminController.php
@@ -9,7 +9,11 @@
 namespace App\Controller\Admin;
 
 
+use App\Classes\Card\CardStatusHelper;
 use App\Classes\Task\TaskAdminController;
+use App\Classes\Task\TaskItemInterface;
+use App\Entity\Card;
+use App\Entity\Equipment;
 use App\Entity\Marking;
 
 class MarkingAdminController extends TaskAdminController
@@ -17,6 +21,24 @@ class MarkingAdminController extends TaskAdminController
     function getEntityClass(): string
     {
         return Marking::class;
+    }
+
+    /**
+     * При завершении задачи, меняем статус карточек на в аренде
+     *
+     * @param TaskItemInterface|Equipment $taskItem
+     * @param $newStatusId
+     */
+    protected function preChangeStatus(TaskItemInterface $taskItem, $newStatusId)
+    {
+        $isComplete = $newStatusId === Marking::STATUS_COMPLETE; // Задача завершена
+        if ($isComplete) {
+            $em = $this->getDoctrine()->getManager();
+            $taskItem->getCards()->map(function (Card $card) use ($em) {
+                $card->setStatus(CardStatusHelper::STATUS_STORE);
+                $em->persist($card);
+            });
+        }
     }
 
 }
