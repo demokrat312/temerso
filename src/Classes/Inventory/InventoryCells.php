32afc7634246f8ae418f2bab3a6cf4cb9f03af8c
oleg <demokrat312@gmail.com>
2020-08-27 13:14:16 +0600

    Учет/Инвентаризация

--- src/Classes/Inventory/InventoryCells.php
+++ src/Classes/Inventory/InventoryCells.php
@@ -44,7 +44,7 @@ class InventoryCells extends ParentCells
             $this->sheet->setCellValue('D' . $rowCount, $card->getCouplingSerialNumber()); // Серийный № муфты
             $this->sheet->setCellValue('E' . $rowCount, $card->getRfidTagNo()); // № RFID-метки
             $this->sheet->setCellValue('F' . $rowCount, $card->getAccounting() ? 'есть' : 'нет'); // Учет/Инвентаризация
-            $this->sheet->setCellValue('G' . $rowCount, $card->getTaskCardOtherFieldsByTask($task)->getComment()); //  Оборудование есть,проблема с меткой
+            $this->sheet->setCellValue('G' . $rowCount, $card->getTaskCardOtherFieldsByTask($task)->getCommentProblemWithMark()); //  Оборудование есть,проблема с меткой
 
             $rowCount++;
         });
