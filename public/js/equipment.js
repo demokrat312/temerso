c21463168f4c57955458f136b0fe9f0e92497588
oleg <demokrat312@gmail.com>
2020-09-04 12:51:51 +0600

    Ошибка создания комплектации в ремонт

--- public/js/equipment.js
+++ public/js/equipment.js
@@ -1,10 +1,15 @@
 (function ($) {
     $(document).ready(function () {
-        EquipmentModel.init();
+        // EquipmentModel.init();
     });
 
     const EquipmentModel = (function () {
-        let $formEdit, $formEditParent;
+        let $formEdit, $formEditParent, $formErrorAlert,
+            // Каталог - добавляем карточки или характеристики
+            $withKitInput = null,
+            // Тип комплекта
+            $kitTypeInput = null
+        ;
         const errorTemplate = `
             <div id="form-alert-danger" class="alert alert-danger" role="alert">
               {message}
@@ -15,24 +20,59 @@
             console.info('EquipmentModel init');
             $formEditParent = $('.sonata-ba-form');
             $formEdit = $formEditParent.find('form:first');
+            $kitTypeInput = $('[name$="[kitType]"]'); // Выбрать тип комплекта
+            $withKitInput = $('[name$="[withKit]"]'); // Каталог
 
             initEvent();
         };
 
         const initEvent = () => {
-            $formEdit.on('submit', onFormEditSubmit )
+            $formEdit.on('submit', onFormEditSubmit);
         };
 
-        const onFormEditSubmit = function(e) {
+        const onFormEditSubmit = function (e) {
             e.preventDefault();
-
             console.log('submit');
-            showError($formEditParent, 'ошибка')
+
+            // С выборкой из каталога
+            if ($withKitInput.val() === 'withCatalog') {
+                if ($kitTypeInput.val() === 'single') {
+                    console.log('выбираем карточки. Количество комплектов ' + 1 + ' количество карточек ' + getInput('cardCount').val())
+                } else {
+                    console.log('выбираем карточки. Количество комплектов ' + getInput('kitCount').val() + ' количество карточек ' + getInput('kitCardCount').val())
+                }
+            } else {
+                console.log('Указываем технические характеристики');
+            }
+
+            showError($formEditParent, 'ошибка');
         };
 
-        const showError = ($element, message) =>  {
-            $element.prepend($(errorTemplate.replace('{message}', message)));
+        const showError = ($element, message) => {
+            hideError();
+            $formErrorAlert = $(errorTemplate.replace('{message}', message));
+            $element.prepend($formErrorAlert);
+            $formErrorAlert[0].scrollIntoView({block: "center", behavior: "smooth"});
         };
+
+        const hideError = () => {
+            $formErrorAlert && $formErrorAlert.remove();
+        };
+
+        const getInput = (name) => {
+            return $formEdit.find('[name$="[' + name + ']"]');
+        };
+
+        const getFormData = ($form) => {
+            const formArray = $form.serializeArray();
+            var returnArray = {};
+            for (var i = 0; i < formArray.length; i++) {
+                const formName = formArray[i]['name'].replace(/.*\[(\w+)\].*/, '$1');
+                returnArray[formName] = formArray[i]['value'];
+            }
+            return returnArray;
+        };
+
 
         return {
             init
