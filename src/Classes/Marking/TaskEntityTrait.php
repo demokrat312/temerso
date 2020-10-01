b37f8458a43cb6323c796ca317d39525d9acd485
oleg <demokrat312@gmail.com>
2020-10-01 13:05:10 +0600

    Ошибка если не указывают исполнителя

--- src/Classes/Marking/TaskEntityTrait.php
+++ src/Classes/Marking/TaskEntityTrait.php
@@ -169,6 +169,7 @@ trait TaskEntityTrait
 
     public function __toString()
     {
+        if(!$this->getCreateAt()) return 'error';
 //        return $this->createdBy ? (string)$this->createdBy->getFio() : 'error';
         return sprintf('%s, %s', TaskItem::TYPE_TITLE[$this->getTaskTypeId()], $this->getCreateAt()->format('d.m.Y'));
     }
